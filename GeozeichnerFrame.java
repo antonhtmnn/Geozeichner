28219ce571661106659b8c11ee5c8784a8e96cef
Anton Hartmann <anton.htmnn@gmail.com>
2021-02-05 19:31:52 +0100

    Main Klasse erzeugt nun ein Objekt der GeozeichnerFrame Klasse. GeozeichnerFrame enthaelt einen Konstruktor mit basic Infos wie z.B die size!

--- GeozeichnerFrame.java
+++ GeozeichnerFrame.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class GeozeichnerFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class GeozeichnerFrame extends JFrame {
+
+    // Konstruktor
+    public GeozeichnerFrame() {
+
+        setTitle("Geozeichner");
+        setLayout(null);
+
+        setSize(1200, 1000);
+        setMinimumSize(new Dimension(600, 500));
+        setResizable(true);
+        setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
