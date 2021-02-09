0fd3d48f9bbcb28f8460d7e5c819b7fd964cd5d8
Anton Hartmann <anton.htmnn@gmail.com>
2021-02-09 17:19:08 +0100

    Das Grobe GUI-Layout wurde erstellt.

---
+++ src/com/company/CenterPanelKoordinatensystem.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CenterPanelKoordinatensystem extends JPanel {
+
+    // Konstruktor
+    public CenterPanelKoordinatensystem() {
+
+        //-------------------- GENERAL --------------------
+        setBackground(Color.BLUE);
+        setLayout(null);
+        //-------------------------------------------------
+
+        //-------------------------------------------------
+        setVisible(true);
+        //-------------------------------------------------
+    }
+}
